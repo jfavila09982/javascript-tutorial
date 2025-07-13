cc252ae4b116541a486d1aaf5752c8637ef268c2
jfavila09982 <jfavila09982@gmail.com>
2025-07-13 12:21:13 +0800

    - store variable chagent content using variable

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let age = 25;
 let student = false;
 
 
-document.getElementById("p1").textContent = fullName;
-document.getElementById("p2").textContent = age;
-document.getElementById("p3").textContent = student;
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
 
