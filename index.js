b4de4c7346ff245755633a638a1b31d27cd9b234
jfavila09982 <jfavila09982@gmail.com>
2025-07-13 19:19:03 +0800

    change text content using user input

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-// document.getElementById("myh1").textContent = `Hello`;
-// document.getElementById("myP").textContent = `I like pizza`;
-
-// variable a container that restores a value 
-//  behave as if it were the value it contains
-
-// 1. declaration let x;
-// 2. assignement x = 100;
-// let age = 25;
-// let price = 10.99;
-// let gpa = 2.1;
 
 
-// let firstName = "Bro";
+// How to accept user input
 
-// console.log(typeof firstName);
+// 1. Easy way = window prompt
+// 2. Better way = HTML textbox
 
-
-let fullName = "Bro Code";
-let age = 25;
-let student = false;
+let username;
 
 
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `You are ${age} years old`;
-document.getElementById("p3").textContent = `Enrolled: ${student}`;
-
+document.getElementById("mySubmit").onclick = function() {
+    username = document.getElementById("myText").value;
+    document.getElementById("h1").textContent = `Hello ${username}`;
+   
+}
+if(username !== undefined) { 
+    document.getElementById("userInput")
+}
