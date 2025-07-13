e686fd892f192b4b34092975e6030fb0a88e3ce2
jfavila09982 <jfavila09982@gmail.com>
2025-07-13 12:06:30 +0800

    condition of text content

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+// document.getElementById("myh1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I like pizza`;
+
+// variable a container that restores a value 
+//  behave as if it were the value it contains
+
+// 1. declaration let x;
+// 2. assignement x = 100;
+let x;
+x = 123;
+
+console.log(x); // 123
+
+if (document.getElementById("myh1").textContent == `Hello`){
+    document.getElementById("myh1").textContent = `Hello World`;
+}else{
+    document.getElementById("myh1").textContent = `World`;
+}
 
 
-document.getElementById("myh1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza`;
