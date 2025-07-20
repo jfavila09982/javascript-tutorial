4c4cc0c496b16c83f5d02fb3a0b21c8d75b5f5ba
jfavila09982 <jfavila09982@gmail.com>
2025-07-20 20:43:22 +0800

    math

---
+++ math/index.js
@@ -0,0 +1,21 @@
+//Math = built-in object that provides
+//        collection of properties and methods
+
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+//z = Math.round(x); // Rounds to the nearest integer
+//z = Math.floor(x); // Rounds down to the nearest integer
+//z = Math.ceil(x); // Rounds up to the nearest integer
+//z = Math.trunc(x); // Removes the decimal part
+//z = Math.power(x,y); // Raises x to the power of y
+//z = Math.sqrt(x); // Square root of x
+//z = Math.sin(x); // Sine of x (x in radians)
+//z = Math.cos(x); // Cosine of x (x in radians)
+//z = Math.abs(x); // Absolute value of x
+//z = Math.sign(x); // Sign of x (-1 for negative, 0 for zero, 1 for positive)
+let max = Math.max(x, y, z); // Maximum value among x, y, z
+let min = Math.min(x, y, z); // Minimum value among x, y, z
+console.log(min); // 3
