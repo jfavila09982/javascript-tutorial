7536c0f58b429ee232b56e14c751b9e7cfad94f2
jfavila09982 <jfavila09982@gmail.com>
2025-07-22 23:23:03 +0800

    switch case

---
+++ switch_case/index.js
@@ -0,0 +1,62 @@
+// SWITCH = can be an efficient replacement to many else if statements
+
+//let day = "pizza"
+// let day = 1;
+
+// switch(day){
+//     case 1:
+//         console.log("It is Monday");
+//         break;
+//     case 2:
+//         console.log("It is a Tuesday");
+//         break;
+//     case 3:
+//         console.log("It is a Wednesday");
+//         break;
+//     case 4:
+//         console.log("It is a Thursday");
+//         break;
+//     case 5:
+//         console.log("It is a Friday");
+//         break;
+//     case 6:
+//         console.log("It is a Saturday");
+//         break;
+//     case 7:
+//         console.log("It is a Sunday");
+//         break;
+//     default:
+//         console.log(`${day} is not a day`);
+    
+// }
+
+
+//Example 2 
+
+// let testScore = 50;
+// let letterGrade;
+
+// switch(true){
+//     case testScore >= 90:
+//         letterGrade = "A";
+//         break;
+//     case testScore >= 80:
+//         letterGrade = "B";
+//         break;
+//     case testScore >= 70:
+//         letterGrade = "C";
+//         break;
+//     case testScore >= 60:
+//         letterGrade = "D";
+//         break;
+//     default:
+//         letterGrade = "F";
+// }
+
+// console.log(letterGrade);
+
+// With HTML 
+
+
+const myCheckBox = document.getElementById("inputGradesMath");
+const visaBtn = document.getElementById("visaBtn");
